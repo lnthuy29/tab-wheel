3879480041f86b27c7abc178ad235af61a8cc210
Lê Nguyễn Trường Huy <lnthuy29@gmail.com>
2025-10-18 10:33:48 +0700

    fix: log in

--- src/app/screens/log-in/log-in-screen.component.ts
+++ src/app/screens/log-in/log-in-screen.component.ts
@@ -1,8 +1,13 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { supabase } from '@supabase';
+import { LoadingState } from 'src/app/models/loading-state.enum';
 
 @Component({
   selector: 'app-log-in-screen',
@@ -10,9 +15,20 @@ import { supabase } from '@supabase';
   styleUrl: './log-in-screen.component.scss',
 })
 export class LogInScreenComponent implements OnInit {
+  protected loadingState: LoadingState =
+    LoadingState.INITIAL;
+
+  protected LoadingState = LoadingState;
+
   protected form: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(2),
+    ]),
   });
 
   public constructor(
@@ -32,18 +48,24 @@ export class LogInScreenComponent implements OnInit {
 
   protected async onSubmit() {
     if (this.form.valid) {
-      const { error } = await supabase.auth.signInWithPassword({
-        email: this.form.value.email!,
-        password: this.form.value.password!,
-      });
+      this.loadingState = LoadingState.LOADING;
+
+      const { error } =
+        await supabase.auth.signInWithPassword({
+          email: this.form.value.email!,
+          password: this.form.value.password!,
+        });
 
       if (error) {
+        this.loadingState = LoadingState.INITIAL;
+
         this.toastr.error(error.message, undefined, {
           timeOut: 3000,
           progressBar: true,
           positionClass: 'toast-bottom-center',
         });
       } else {
+        this.loadingState = LoadingState.LOADED;
         this.router.navigate(['/protected']);
       }
     }
