c7cf2f59898e247c42dd816b20f2c0162132e6a3
Lê Nguyễn Trường Huy <lnthuy29@gmail.com>
2025-10-19 09:46:18 +0700

    fix: change password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  OnDestroy,
+} from '@angular/core';
 import { AppState } from './app.state';
 import { Store } from '@ngrx/store';
 import { AuthService } from './services/auth.service';
 import { setUserProfile } from './store/profile/profile.action';
+import { UserProfile } from './models/profile.interface';
+import { ChangePasswordModalComponent } from './components/change-password-modal/change-password-modal.component';
+import { Subscription } from 'rxjs';
+import { selectProfile } from './store/profile/profile.selector';
+import { Nullable } from './models/nullable.type';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
+  private profile!: Nullable<UserProfile>;
+  private profileSub!: Subscription;
+
+  @ViewChild(ChangePasswordModalComponent)
+  public modal!: ChangePasswordModalComponent;
+
   public constructor(
     private store: Store<AppState>,
     private authService: AuthService,
@@ -16,6 +32,13 @@ export class AppComponent implements OnInit {
 
   public ngOnInit(): void {
     this.loadUserProfile();
+
+    this.profileSub = this.store
+      .select(selectProfile)
+      .subscribe((profile) => {
+        this.profile = profile;
+        this.showChangePasswordModalIfNeeded();
+      });
   }
 
   private async loadUserProfile() {
@@ -30,5 +53,18 @@ export class AppComponent implements OnInit {
         this.store.dispatch(setUserProfile({ profile }));
       }
     }
+  }
+
+  private showChangePasswordModalIfNeeded(): void {
+    if (!this.profile?.passwordChangedFirstTime) {
+      console.log('Open modal');
+      this.modal?.open();
+    } else {
+      this.modal?.close();
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.profileSub?.unsubscribe();
   }
 }
