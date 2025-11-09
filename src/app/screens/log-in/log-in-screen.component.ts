fcee0fc76794714f051857c3b8c8fb3e06a9c38a
lnthuy29 <lnthuy29@gmail.com>
2025-11-09 03:42:57 +0100

    feat: link to sign up screen

--- src/app/screens/log-in/log-in-screen.component.ts
+++ src/app/screens/log-in/log-in-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
