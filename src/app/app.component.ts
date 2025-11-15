5db4e3ad8f654af0e6475688888186e6b5142aac
lnthuy29 <lnthuy29@gmail.com>
2025-11-15 09:11:05 +0700

    fix: remove change password first time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,11 @@ import {
   ViewChild,
   OnDestroy,
 } from '@angular/core';
-import { AppState } from './store/app.state';
 import { Store } from '@ngrx/store';
-import { AuthService } from './services/auth.service';
+import { Subscription } from 'rxjs';
+import { AppState } from './store/app.state';
 import { setUserProfile } from './store/profile/profile.action';
-import { UserProfile } from './models/profile.interface';
-import { ChangePasswordModalComponent } from './components/change-password-modal/change-password-modal.component';
-import { filter, Subscription, take, tap } from 'rxjs';
-import { selectProfile } from './store/profile/profile.selector';
-import { Nullable } from './models/nullable.type';
-import { ModalConfiguration } from './components/modal/models/modal.interface';
-import { ModalSize } from './components/modal/models/modal-size.enum';
+import { AuthService } from './services/auth.service';
 import { ProfileHelper } from './helpers/profile.helper';
 
 @Component({
@@ -26,17 +20,6 @@ export class AppComponent
   implements OnInit, OnDestroy
 {
   private subscription: Subscription = new Subscription();
-
-  @ViewChild(ChangePasswordModalComponent)
-  public modal!: ChangePasswordModalComponent;
-
-  protected modalConfiguration: ModalConfiguration = {
-    title: 'Change password',
-    subtitle:
-      'As this is your first login, we recommend you change your password to protect your privacy.',
-    showCloseButton: false,
-    size: ModalSize.SMALL,
-  };
 
   public constructor(
     store: Store<AppState>,
@@ -64,28 +47,6 @@ export class AppComponent
       if (profile) {
         this.store.dispatch(setUserProfile({ profile }));
       }
-    }
-    this.subscription.add(
-      this.profile$
-        .pipe(
-          filter(
-            (p: Nullable<UserProfile>): p is UserProfile =>
-              !!p,
-          ),
-          take(1),
-          tap(() => this.showChangePasswordModalIfNeeded()),
-        )
-        .subscribe(),
-    );
-  }
-
-  private showChangePasswordModalIfNeeded(): void {
-    if (!this.profile) return;
-
-    if (!this.profile?.passwordChangedFirstTime) {
-      this.modal?.open();
-    } else {
-      this.modal?.close();
     }
   }
 }
